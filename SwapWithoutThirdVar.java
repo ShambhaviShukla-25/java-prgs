ba462671333e8cc3e922917ace0090d35a28768d
ShambhaviShukla-25 <bit23it28@bit.ac.in>
2025-05-04 10:59:26 +0530

    Create SwapWithoutThirdVar.java

---
+++ SwapWithoutThirdVar.java
@@ -0,0 +1,29 @@
+
+//This program swaps two numbers without using a third variable by taking user input
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class SwapWithoutThirdVar {
+    public void swap() {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Enter the 1st number:");
+            int no1 = Integer.parseInt(br.readLine());
+            System.out.print("Enter the 2nd number:");
+            int no2 = Integer.parseInt(br.readLine());
+            System.out.println("The numbers before swapping are:" + no1 + " and " + no2);
+            no1 = no1 + no2;
+            no2 = no1 - no2;
+            no1 = no1 - no2;
+            System.out.println("The numbers after swapping are:" + no1 + " and " + no2);
+        } catch (IOException e) {
+            System.out.println("There is an error:" + e.getMessage());
+        }
+    }
+
+    public static void main(String args[]) {
+        SwapWithoutThirdVar s = new SwapWithoutThirdVar();
+        s.swap();
+    }
+}
